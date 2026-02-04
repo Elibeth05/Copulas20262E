2d1e20db3f0377a8071e2c4a1558692f6b98e1b4
Elibeth05 <marthsz.elizabeth@gmail.com>
2026-02-04 01:18:43 -0600

    Refactor distprob and masaprob functions for clarity

--- 02probestim.jl
+++ 02probestim.jl
@@ -3,7 +3,7 @@
 ### versión: 2025-12-28
 
 ### Funciones:  distprob  masaprob  densprob
-
+#? y enter para ayuda
 
 """
     distprob(muestra::Vector{<:Real})
@@ -17,7 +17,7 @@ Entrega una tupla etiquetada con los siguientes nombres:
 
 ## Ejemplo
 ```
-muestra = randn(10_000);
+muestra = randn(10_000);# ; para que no despliegue ka muestra en pantalla
 D = distprob(muestra);
 D.min, D.max
 D.mord
@@ -26,15 +26,15 @@ F(0)
 diff(F.([-1.96, 1.96]))
 ```
 """
-function distprob(muestra::Vector{<:Real})
-    Fn(x) = count(muestra .≤ x) / length(muestra)
-    inf, sup = extrema(muestra) # mínimo y máximo muestrales
+function distprob(muestra::Vector{<:Real})#control sobre lo que entra, un vector sobre los reales
+    Fn(x) = count(muestra .≤ x) / length(muestra)#en la fn de distrib empirica
+    inf, sup = extrema(muestra) # mínimo y máximo muestrales, como tupla de valreores
     return (fda = Fn, min = inf, max = sup, mord = sort(muestra))
 end
 
 
 """
-    masaprob(muestra::Vector)
+    masaprob(muestra::Vector)#no necesario reales, reales solo en v.a.
 
 Estima la función de masa de probabilidades a partir del vector `muestra`.
 Entrega una tupla etiquetada con los siguientes nombres:
@@ -47,14 +47,14 @@ Entrega una tupla etiquetada con los siguientes nombres:
 ```
 muestra = rand(['a','b','c'], 100);
 M = masaprob(muestra);
-hcat(M.valores, M.probs) # tabla
+hcat(M.valores, M.probs) # tabla, cncatenacion horizontal
 M.fmp('c')
 M.fmp.(['a', 'b'])
 sum(M.fmp.(M.valores)) # deber ser 1.0 (aprox)
 ```
 """
 function masaprob(muestra::Vector)
-    valores = unique(muestra) # valores distintos de la muestra
+    valores = unique(muestra) # valores distintos de la muestra, te da otro vector
     try 
         sort!(valores) # intenta ordenar valores
     catch
@@ -65,7 +65,7 @@ function masaprob(muestra::Vector)
     n = length(muestra) # tamaño de la muestra
     for i ∈ 1:nv # calcular frecuencia absoluta de los distintos valores de la muestra
         for j ∈ 1:n
-            if valores[i] == muestra[j]
+            if valores[i] == muestra[j]#verificamos si el valor i'esimo distinto es igual a el valor i'esimo de la muestra
                 frecuencia[i] += 1
             end
         end
